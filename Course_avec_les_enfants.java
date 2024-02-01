a91770a9625b2eaec57e7c8791fc0f0bd2d5ec57
Mohamed Ali Farhat <100702507+MohamedAliFathat@users.noreply.github.com>
2024-02-01 14:23:40 +0000

    solution

---
+++ Course_avec_les_enfants.java
@@ -0,0 +1,43 @@
+import static algorea.Robot.*;
+
+class Main {
+    public static void main(String[] args)
+     {
+     int n=1;   
+
+
+
+for(int j=0;j<10;j++)
+
+{
+
+
+for (int i=0;i<n;i++)
+{ droite(); }
+
+ramasser();
+
+for (int i=0;i<n;i++)
+{ gauche(); }
+
+deposer();
+
+
+n++;
+
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+}
